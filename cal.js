e3b11e41832453cd12f30ac83e57a8f2166ca433
amiekhb <aminaaami0715@gmail.com>
2024-07-05 11:45:55 +0800

    a

--- cal.js
+++ cal.js
@@ -1,32 +1,29 @@
 const display = document.getElementById("display");
 const numberBtn = document.querySelectorAll(".btnn");
 const c = document.querySelector(".c");
-const btnOperation = document.querySelectorAll(".operation");
-// const plus = document.querySelector(".plus");
-// const minus = document.querySelector(".minus");
-// const divide = document.querySelector(".divide");
-// const multiply = document.querySelector(".multiply");
+const operation = document.querySelectorAll(".operation");
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const divide = document.querySelector(".divide");
+const multiply = document.querySelector(".multiply");
 const equal = document.querySelector(".equal");
-// let current = "";
+let current = "";
 // let previous = "";
 
 //too displayd gargah function
-let operator= ""
-let a =0
 for (let i = 0; i < numberBtn.length; i++) {
   numberBtn[i].addEventListener("click", function () {
     console.log("click", numberBtn[i].innerHTML);
     display.textContent += numberBtn[i].innerHTML;
 
-    // current = display.textContent;
-    // console.log("current", current);
+    current = display.textContent;
+    console.log("current", current);
     // // "1" + "2" , display += "3" 23
     // // display += numberBtn[i].innerHTML; // <div class="con" id=""></div> + "1"
     // const currNumb = Number(current);
     // console.log("current", currNumb);
   });
 }
-console.log(numberBtn);
 
 // arilgah function
 c.addEventListener("click", function () {
@@ -34,26 +31,31 @@ c.addEventListener("click", function () {
 });
 
 // operation darahad prev bolgoj hadgalaad arilgah
-for (let i = 0; i < btnOperation.length; i++) {
-  btnOperation[i].addEventListener("click", () => {
-    operator=btnOperation[i].textContent
-    a=display.textContent
+let operator = "";
+let a = 0;
+for (let i = 0; i < operation.length; i++) {
+  operation[i].addEventListener("click", () => {
+    operator = operation[i].textContent;
+    a = current;
     display.textContent = "";
     // previous = current;
 
     // const prevNumb = Number(previous);
     // console.log("previous", prevNumb);
+    return a;
   });
+
+  console.log("saved");
 }
 //nemeh function
 equal.addEventListener("click", function () {
-  let answer = 0;
-  if (operator==="+"){
-    result= Number(a)+Number(display.textContent);
-  }else if (operator==="-"){
-    result= Number(a)+Number(display.textContent)
+  let result = 0;
+  if (operator === "+") {
+    result = Number(a) + Number(display.textContent);
+  } else if (operator === "-") {
+    result = Number(a) + Number(display.textContent);
   }
-  display.textContent=result
+  display.textContent = result;
 });
 
 // minus.addEventListener("click", function () {});
